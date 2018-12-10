004b6e29834595fae3c7eebf3a198d08c5e1dd44
= <chase.terry87@gmail.com>
2018-12-10 13:06:19 -0600

    Setup our first unit test for threads

--- tdd-laravel-forum/forum/routes/web.php
+++ tdd-laravel-forum/forum/routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadController@index');
+Route::get('/threads/{thread}', 'ThreadController@show');
