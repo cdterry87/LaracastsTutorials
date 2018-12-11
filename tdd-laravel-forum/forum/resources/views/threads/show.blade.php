87bfbc5f4f407d0d827c6ebb3ec4a057c87fa015
= <chase.terry87@gmail.com>
2018-12-11 12:16:48 -0600

    Created test for viewing replies associated to a thread

--- tdd-laravel-forum/forum/resources/views/threads/show.blade.php
+++ tdd-laravel-forum/forum/resources/views/threads/show.blade.php
@@ -15,6 +15,25 @@
                 </div>
             </div>
         </div>
+
+        <hr>
+
+        <h3>Replies</h3>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach($thread->replies as $reply)
+                    <div class="panel panel-header">
+                        {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                    <hr>
+                @endforeach
+            </div>
+        </div>
     </div>
 
 @endsection
--- tdd-laravel-forum/forum/tests/Feature/ThreadsTest.php
+++ tdd-laravel-forum/forum/tests/Feature/ThreadsTest.php
@@ -9,24 +9,37 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
         $response->assertStatus(200);
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_view_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads/' . $thread->id);
+        $response = $this->get('/threads/' . $this->thread->id);
         $response->assertStatus(200);
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_associated_to_a_thread()
+    {
+        // We have a thread and the thread includes replies
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        // When we visit a thread page we should see replies
+        $this->get('/threads/' . $this->thread->id)->assertSee($reply->body);
     }
 
 }
